92b34affc0390ecf3d072ec4c6b59ef2819d0f46
René Mundt <renem@live.dk>
2017-10-17 00:01:15 +0200

    feat(expenses): fetch expenses from backend

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,53 +1,34 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <h1>Latest expenses</h1>
+    <div v-for="expense in expenses" :key="expense.id">
+      <div>{{ expense.store }} || {{ expense.amount }} || {{ expense.timeOfPurchase }}</div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Expenses',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      expenses: []
     }
+  },
+  created: function () {
+    this.$http.get('http://localhost:8666/api/expenses').then(response => {
+      this.expenses = response.data
+    }, error => {
+      console.log('error', error)
+    })
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
 }
 </style>
