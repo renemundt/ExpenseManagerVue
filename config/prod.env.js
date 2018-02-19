60d8d4abf2d7d1287deaf04baacde3d65b98086c
René Mundt <renem@live.dk>
2018-02-19 15:40:23 +0100

    chore(urls): update with correct urls

--- config/prod.env.js
+++ config/prod.env.js
@@ -4,10 +4,10 @@ module.exports = {
   AUTH: {
     CLIENT_ID: '"z3ezLq6XG2agekCwBHBkZMCxWy6cBw2O"',
     CLIENT_DOMAIN: '"renemundt.eu.auth0.com"',
-    REDIRECT: '"http://when-decided-where-to-put-it/callback"',
+    REDIRECT: '"https://expensemanagerfrontend.azurewebsites.net/callback"',
     SCOPE: '"openid profile full_access"',
     AUDIENCE: '"ExpenseManagerFrontends"'
   },
   OLD_API_URL: '"https://expense-manager-backend.herokuapp.com/api"',
-  AZURE_API_URL: '"https://expensemanagerbackend.azurewebsites.net/api"'
+  API_URL: '"https://expensemanagerbackend.azurewebsites.net/api"'
 }
