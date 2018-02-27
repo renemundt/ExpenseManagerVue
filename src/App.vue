279467b8b0b9da4b31ff5d69df19ea24b3586bc3
René Mundt <renem@live.dk>
2018-02-27 21:37:43 +0100

    chore(test): test that CI works

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@ export default {
 
 <style>
 #app {
-  background-color: lightgoldenrodyellow;
+  background-color: red;
   /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
