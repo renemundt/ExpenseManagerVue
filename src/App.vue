2bc6576af70d789258cfd7a7995bcf2354ed778c
René Mundt <renem@live.dk>
2018-02-28 13:24:26 +0100

    chore(test): test CI/CD 3

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,7 @@ export default {
 
 <style>
 #app {
+  background-color: chocolate
   /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
