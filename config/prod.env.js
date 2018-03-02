3c50bdc908ac59796534fa40449479e604f037fa
René Mundt <renem@live.dk>
2018-03-02 15:24:02 +0100

    chore(revert): use azure site directly

--- config/prod.env.js
+++ config/prod.env.js
@@ -4,8 +4,8 @@ module.exports = {
   AUTH: {
     CLIENT_ID: '"z3ezLq6XG2agekCwBHBkZMCxWy6cBw2O"',
     CLIENT_DOMAIN: '"renemundt.eu.auth0.com"',
-    REDIRECT_AZURE: '"https://expensemanagerfrontend.azurewebsites.net/callback"',
-    REDIRECT: '"http://expensemanager.renemundt.com/callback"',
+    REDIRECT: '"https://expensemanagerfrontend.azurewebsites.net/callback"',
+    REDIRECT_RENEMUNDT: '"http://expensemanager.renemundt.com/callback"',
     SCOPE: '"openid profile full_access"',
     AUDIENCE: '"ExpenseManagerFrontends"'
   },
